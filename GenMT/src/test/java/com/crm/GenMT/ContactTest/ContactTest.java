2fbfa57d50eaa01e705dcc6780e03363f1de81bf
SachinsachU <SachinsachU@My-Pc>
2021-03-15 13:06:03 +0530

    framework

---
+++ GenMT/src/test/java/com/crm/GenMT/ContactTest/ContactTest.java
@@ -0,0 +1,22 @@
+package com.crm.GenMT.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("exicute create contact");
+	}
+	@Test
+	public void modifycontacttest()
+	{
+		System.out.println("mofify contact");
+	}
+	@Test
+	public void deletecontacttest()
+	{
+		System.out.println("delete contact");
+	}
+}
